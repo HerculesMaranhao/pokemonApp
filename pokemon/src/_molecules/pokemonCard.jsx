f2c6ce43439e7899cecabdeb809703c8d4a107ea
marcosapaalves <marcos.apa.alves@outlook.com>
2020-07-02 22:32:09 -0300

    Projeto Pokemon

---
+++ pokemon/src/_molecules/pokemonCard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import Card from './../_atoms/card'
+
+class PokemonCard extends Component {
+
+  render(){
+
+    return(
+      <div>
+        <Card />
+        <Card />
+      </div>
+    )
+  }
+}
+
+export default PokemonCard
---
+++ pokemon/src/_organisms/pokemonsList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import PokemonCard from './../_molecules/pokemonCard'
+
+class PokemonList extends Component {
+
+  render(){
+
+    return(
+      <div>
+        <PokemonCard />
+      </div>
+    )
+  }
+}
+
+export default PokemonList
